78762860e2ffac9a07acabb723e44abc939e8c3c
Ben Meier <benmeier42@gmail.com>
2014-02-25 16:14:22 +0200

    moved into class and main func

--- ndltd-scraper.rb
+++ ndltd-scraper.rb
@@ -7,64 +7,76 @@
 
 require 'net/http'
 
-base_url = 'http://union.ndltd.org/OAI-PMH/'
+class NDLTDScraper
+    def initialize(storage_dir)
+        @storage_dir = storage_dir
+    end
 
-file_id = 0
+    def run
+        base_url = 'http://union.ndltd.org/OAI-PMH/'
 
-resumption_token = nil
+        file_id = 0
 
-bytes_received = 0
+        resumption_token = nil
 
-storage_dir = nil
+        bytes_received = 0
 
-# arg handling
-if ARGV.length == 0 or ARGV[0].length == 0
-    raise ArgumentError, 'Expected output directory argument'
-else
-    storage_dir = File.join(Dir.pwd, ARGV[0])
-    if not Dir.exists?(storage_dir)
-        Dir.mkdir(ARGV[0])
+        begin
+            uri = URI("#{base_url}?verb=ListRecords&" + ( (resumption_token) ? "resumptionToken=#{resumption_token}" : 'metadataPrefix=oai_dc' ) )
+
+            puts "Harvesting #{uri}"
+
+            res = Net::HTTP.get_response(uri)
+            while res.code == 503
+                sleep_time = res['Retry-After'].to_i
+                if sleep_time >= 0 and sleep_time <= 86400
+                    puts "Sleeping for #{sleep_time}s"
+                    sleep(sleep_time)
+                    res = Net::HTTP.get_response(uri)
+                else
+                    raise ArgumentError, "invalid sleep time #{res['Retry-After']}"
+                end
+            end
+
+            content = res.body
+
+            puts "Got response of #{content.length} bytes"
+
+            bytes_received += content.length
+
+            file = File.join(@storage_dir, "data_#{file_id}.xml")
+
+            puts "Writing to #{file}"
+            File.open(file, 'w') {|f| f.write(content)}
+
+            file_id += 1
+
+            resumption_token = nil
+            if content =~ /<resumptionToken[^>]*>([^<]+)<\/resumptionToken>/
+                puts "Detected resumption token"
+                resumption_token = $1
+            end
+
+        end while resumption_token
+
+        puts "Final bytes: #{bytes_received}"
     end
 end
 
-begin
-    uri = URI("#{base_url}?verb=ListRecords&" + ( (resumption_token) ? "resumptionToken=#{resumption_token}" : 'metadataPrefix=oai_dc' ) )
+if __FILE__ == $PROGRAM_NAME
+    storage_dir = nil
 
-    puts "Harvesting #{uri}"
-
-    res = Net::HTTP.get_response(uri)
-    while res.code == 503
-        sleep_time = res['Retry-After'].to_i
-        if sleep_time >= 0 and sleep_time <= 86400
-            puts "Sleeping for #{sleep_time}s"
-            sleep(sleep_time)
-            res = Net::HTTP.get_response(uri)
-        else
-            raise ArgumentError, "invalid sleep time #{res['Retry-After']}"
+    # arg handling
+    if ARGV.length == 0 or ARGV[0].length == 0
+        raise ArgumentError, 'Expected output directory argument'
+    else
+        storage_dir = File.join(Dir.pwd, ARGV[0])
+        if not Dir.exists?(storage_dir)
+            Dir.mkdir(ARGV[0])
         end
     end
 
-    content = res.body
+    d = NDLTDScraper.new(storage_dir)
+    d.run()
 
-    puts "Got response of #{content.length} bytes"
-
-    bytes_received += content.length
-
-    file = File.join(storage_dir, "data_#{file_id}.xml")
-
-    puts "Writing to #{file}"
-    File.open(file, 'w') {|f| f.write(content)}
-
-    file_id += 1
-
-    resumption_token = nil
-    if content =~ /<resumptionToken[^>]*>([^<]+)<\/resumptionToken>/
-        puts "Detected resumption token"
-        resumption_token = $1
-    end
-
-
-
-end while resumption_token
-
-puts "Final bytes: #{bytes_received}"
+end
