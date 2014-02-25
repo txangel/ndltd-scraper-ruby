c3fdb920388786f1d3d860fa579d257cc3d12cda
Ben Meier <benmeier42@gmail.com>
2014-02-25 16:04:09 +0200

    ruby translation of original harvester perl script

---
+++ ndltd-scraper.rb
@@ -0,0 +1,70 @@
+# Ruby version of the Harvey harvester script
+# ----------------------------------------------------------------------------
+# TODO: support updates by including command line options for ListRecord from
+# and until params.
+#
+# ----------------------------------------------------------------------------
+
+require 'net/http'
+
+base_url = 'http://union.ndltd.org/OAI-PMH/'
+
+file_id = 0
+
+resumption_token = nil
+
+bytes_received = 0
+
+storage_dir = nil
+
+# arg handling
+if ARGV.length == 0 or ARGV[0].length == 0
+    raise ArgumentError, 'Expected output directory argument'
+else
+    storage_dir = File.join(Dir.pwd, ARGV[0])
+    if not Dir.exists?(storage_dir)
+        Dir.mkdir(ARGV[0])
+    end
+end
+
+begin
+    uri = URI("#{base_url}?verb=ListRecords&" + ( (resumption_token) ? "resumptionToken=#{resumption_token}" : 'metadataPrefix=oai_dc' ) )
+
+    puts "Harvesting #{uri}"
+
+    res = Net::HTTP.get_response(uri)
+    while res.code == 503
+        sleep_time = res['Retry-After'].to_i
+        if sleep_time >= 0 and sleep_time <= 86400
+            puts "Sleeping for #{sleep_time}s"
+            sleep(sleep_time)
+            res = Net::HTTP.get_response(uri)
+        else
+            raise ArgumentError, "invalid sleep time #{res['Retry-After']}"
+        end
+    end
+
+    content = res.body
+
+    puts "Got response of #{content.length} bytes"
+
+    bytes_received += content.length
+
+    file = File.join(storage_dir, "data_#{file_id}.xml")
+
+    puts "Writing to #{file}"
+    File.open(file, 'w') {|f| f.write(content)}
+
+    file_id += 1
+
+    resumption_token = nil
+    if content =~ /<resumptionToken[^>]*>([^<]+)<\/resumptionToken>/
+        puts "Detected resumption token"
+        resumption_token = $1
+    end
+
+
+
+end while resumption_token
+
+puts "Final bytes: #{bytes_received}"
